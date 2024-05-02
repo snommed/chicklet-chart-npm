ac7981e5048c8a4f6394faaf9b011c974f305035
snommed <dvdmmns@gmail.com>
2024-05-02 00:05:21 -0600

    Add files via upload
    Initial project start

---
+++ rollup.config.js
@@ -0,0 +1,75 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonJs from '@rollup/plugin-commonjs';
+import postCss from 'rollup-plugin-postcss';
+import postCssSimpleVars from 'postcss-simple-vars';
+import postCssNested from 'postcss-nested';
+import babel from '@rollup/plugin-babel';
+import terser from "@rollup/plugin-terser";
+import dts from 'rollup-plugin-dts';
+
+import pkg from './package.json' assert { type: 'json' };
+const { name, homepage, version, dependencies, peerDependencies } = pkg;
+
+const umdConf = {
+  format: 'umd',
+  name: 'TimelinesChart',
+  banner: `// Version ${version} ${name} - ${homepage}`
+};
+
+export default [
+  {
+    input: 'src/index.js',
+    output: [
+      { // umd
+        ...umdConf,
+        file: `dist/${name}.js`,
+        sourcemap: true,
+      },
+      { // minify
+        ...umdConf,
+        file: `dist/${name}.min.js`,
+        plugins: [terser({
+          output: { comments: '/Version/' }
+        })]
+      }
+    ],
+    plugins: [
+      postCss({
+        plugins: [
+          postCssSimpleVars(),
+          postCssNested()
+        ]
+      }),
+      resolve(),
+      commonJs(),
+      babel({ exclude: 'node_modules/**' })
+    ]
+  },
+  { // ES module
+    input: 'src/index.js',
+    output: [
+      {
+        format: 'es',
+        file: `dist/${name}.mjs`
+      }
+    ],
+    external: [...Object.keys(dependencies || {}), ...Object.keys(peerDependencies || {})],
+    plugins: [
+      postCss({
+        plugins: [
+          postCssSimpleVars(),
+          postCssNested()
+        ]
+      }),
+      babel()
+    ]
+  },
+  { // expose TS declarations
+    input: 'src/index.d.ts',
+    output: [{
+      file: `dist/${name}.d.ts`,
+      format: 'es'
+    }],
+    plugins: [dts()]
+  }
+];
